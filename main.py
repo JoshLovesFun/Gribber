76081d1ec1573ec1d741833265c4e2f85f4c1cbd
JoshLovesFun <ihtftl@gmail.com>
2025-03-06 13:39:49 -0500

    rename files

--- main.py
+++ main.py
@@ -5,11 +5,11 @@
 
 import sys
 import os
-import read_control_module
+import read_control
 import file_and_time_control
-import herbie_module
-import grib_module
-import output_module
+import my_herbie
+import grib
+import output
 
 
 def main():
@@ -19,9 +19,9 @@ def main():
     else:
         control_filename = "Control.txt"
 
-    control_data = (read_control_module.read_control_file
+    control_data = (read_control.read_control_file
                     (default_working_directory, control_filename))
-    processed_data = read_control_module.process_control_data(control_data)
+    processed_data = read_control.process_control_data(control_data)
 
     file_and_time_control.expected_file_count(processed_data)
 
@@ -38,33 +38,33 @@ def main():
         print('Invalid input entered for "Flow:" option. Try again.')
         sys.exit(1)
 
-    output_module.print_input_data(processed_data)
+    output.print_input_data(processed_data)
 
     # Write the processed input data to the specified file
-    output_module.write_processed_input_data_to_file(processed_data,
+    output.write_processed_input_data_to_file(processed_data,
                                                      output_file_path)
 
     if processed_data.get('flow_options') in ("h", "hw", "hwa"):
-        herbie_module.fetch_herbie_data_in_range(processed_data)
+        my_herbie.fetch_herbie_data_in_range(processed_data)
     elif processed_data.get('flow_options') == "e":
         main_output = processed_data.get('main_output')
 
-        grib_module.match_strings_and_add_dummy_files(working_directory_grib)
+        grib.match_strings_and_add_dummy_files(working_directory_grib)
 
-        all_files_prs, all_files_nat = grib_module.files_to_do_work_for(working_directory_grib,
+        all_files_prs, all_files_nat = grib.files_to_do_work_for(working_directory_grib,
                                                      processed_data)
-        grid_cell_data = grib_module.process_grib_files(working_directory_main,
+        grid_cell_data = grib.process_grib_files(working_directory_main,
                                                         processed_data,
                                                         all_files_prs,
                                                         all_files_nat)
-        extracted_time_values = grib_module.extract_time_info_from_grib_files(
+        extracted_time_values = grib.extract_time_info_from_grib_files(
             all_files_prs, all_files_nat)
 
-        grib_short_name, grib_level = grib_module.grib_dictionary_from_inputs(
+        grib_short_name, grib_level = grib.grib_dictionary_from_inputs(
             processed_data)
-        build_dict = grib_module.dict_constructor()
+        build_dict = grib.dict_constructor()
 
-        extracted_data = grib_module.extract_value_at_grid_index(
+        extracted_data = grib.extract_value_at_grid_index(
             all_files_prs,
             all_files_nat,
             grid_cell_data,
@@ -74,11 +74,11 @@ def main():
             build_dict
         )
         years, months, days, hours, hours_ending = (
-            output_module.make_all_times(processed_data))
+            output.make_all_times(processed_data))
 
 
         # Creating an instance of the GridDataExtractor class
-        grid_extractor = grib_module.GridDataExtractor(
+        grid_extractor = grib.GridDataExtractor(
             all_files_prs,
             all_files_nat,
             grid_cell_data,
@@ -90,7 +90,7 @@ def main():
         # Calling the method to print values
         grid_extractor.print_values()
 
-        output_module.write_all_data(
+        output.write_all_data(
             years, months, days, hours, hours_ending,
             working_directory_main, main_output, extracted_data
         )
